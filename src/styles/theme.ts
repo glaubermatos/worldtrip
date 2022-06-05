d471fc40f8163999d397e8174dc086f90214dffc
glaubermatos <glaub.oliveira@hotmail.com>
2022-06-05 11:53:40 -0300

    adding chakra ui and setting theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    fonts: {
+        heading: `'Poppins', sans-serif`,
+        body: `'Poppins', sans-serif`,
+        cardTitle: `'Barlow', sans-serif`
+    }
+})
